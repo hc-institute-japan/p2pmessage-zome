6f5b55f5f44d7c24ba7a413c69a9a1cbbd4ec5e0
Neilson Gardose <neilson.keith.gardose@gmail.com>
2020-12-15 22:14:18 +0800

    Push implementation for global and per agent preference. Implement emit_signal for the said preferences

--- zome/zomes/p2pmessage/src/entries/message.rs
+++ zome/zomes/p2pmessage/src/entries/message.rs
@@ -1,8 +1,5 @@
-use hdk3::prelude::{
-    timestamp::Timestamp,
-    *,
-};
 use derive_more::{From, Into};
+use hdk3::prelude::{timestamp::Timestamp, *};
 pub mod handlers;
 
 // currently a public entry because of a bug in committing entries
@@ -13,13 +10,56 @@ pub struct MessageEntry {
     receiver: AgentPubKey,
     payload: String,
     time_sent: Timestamp,
-    time_received: Option<Timestamp>
+    time_received: Option<Timestamp>,
+}
+
+#[hdk_entry(id = "preference", visibility = "private")]
+pub struct Preference {
+    typing_indicator: bool,
+    read_receipt: bool,
+}
+
+#[hdk_entry(id = "per_agent_preference", visibility = "private")]
+pub struct PerAgentPreference {
+    typing_indicator: Vec<AgentPubKey>,
+    read_receipt: Vec<AgentPubKey>,
+}
+
+#[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
+pub struct TypingSignal {
+    kind: String,
+    agent: AgentPubKey,
+    is_typing: bool,
+}
+
+#[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
+pub struct MessageSignal {
+    kind: String,
+    message: MessageOutput,
+}
+
+#[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
+pub enum Signal {
+    Message(MessageSignal),
+    Typing(TypingSignal),
 }
 
 #[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
 pub struct MessageInput {
-    receiver: AgentPubKey, 
-    payload: String
+    receiver: AgentPubKey,
+    payload: String,
+}
+
+#[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
+pub struct PreferenceIO {
+    typing_indicator: Option<bool>,
+    read_receipt: Option<bool>,
+}
+
+#[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
+pub struct PerAgentPreferenceIO {
+    typing_indicator: Option<Vec<AgentPubKey>>,
+    read_receipt: Option<Vec<AgentPubKey>>,
 }
 
 #[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
@@ -28,7 +68,13 @@ pub struct MessageOutput {
     receiver: AgentPubKey,
     payload: String,
     time_sent: Timestamp,
-    time_received: Option<Timestamp>
+    time_received: Option<Timestamp>,
+}
+
+#[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
+pub struct TypingInfo {
+    agent: AgentPubKey,
+    is_typing: bool,
 }
 
 impl MessageEntry {
@@ -38,7 +84,7 @@ impl MessageEntry {
             receiver: message_output.receiver,
             payload: message_output.payload,
             time_sent: message_output.time_sent,
-            time_received: message_output.time_received
+            time_received: message_output.time_received,
         }
     }
 }
@@ -50,7 +96,7 @@ impl MessageOutput {
             receiver: message_entry.receiver,
             payload: message_entry.payload,
             time_sent: message_entry.time_sent,
-            time_received: message_entry.time_received
+            time_received: message_entry.time_received,
         }
     }
 }
@@ -58,16 +104,16 @@ impl MessageOutput {
 #[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
 pub struct MessagesByAgent {
     author: AgentPubKey,
-    messages: Vec<MessageOutput>
+    messages: Vec<MessageOutput>,
 }
 
 #[derive(Serialize, Deserialize, SerializedBytes, Clone, Debug)]
 pub struct MessageRange {
     author: AgentPubKey,
-    last_message_timestamp_seconds: i64
+    last_message_timestamp_seconds: i64,
 }
 
-#[derive(From, Into, Serialize, Deserialize, SerializedBytes)]
+#[derive(From, Into, Serialize, Deserialize, Clone, SerializedBytes)]
 pub struct BooleanWrapper(bool);
 
 #[derive(From, Into, Serialize, Deserialize, SerializedBytes)]
@@ -81,6 +127,12 @@ pub struct AgentListWrapper(Vec<AgentPubKey>);
 
 #[derive(From, Into, Serialize, Deserialize, SerializedBytes)]
 pub struct MessagesByAgentListWrapper(Vec<MessagesByAgent>);
+
+#[derive(From, Into, Serialize, Deserialize, SerializedBytes)]
+pub struct PreferenceWrapper(PreferenceIO);
+
+#[derive(From, Into, Serialize, Deserialize, SerializedBytes)]
+pub struct PerAgentPreferenceWrapper(PerAgentPreferenceIO);
 
 #[derive(Serialize, Deserialize, SerializedBytes, Debug)]
 pub struct Claims(Vec<CapClaim>);
--- zome/zomes/p2pmessage/src/lib.rs
+++ zome/zomes/p2pmessage/src/lib.rs
@@ -3,19 +3,12 @@ mod entries;
 mod utils;
 use entries::message;
 
-use message::{
-    MessageEntry,
-    MessageInput,
-    MessageOutput,
-    MessageOutputOption,
-    MessageListWrapper,
-    AgentListWrapper,
-    MessagesByAgentListWrapper,
-    MessageRange
-};
+use message::*;
 
 entry_defs![
-    MessageEntry::entry_def()
+    MessageEntry::entry_def(),
+    Preference::entry_def(),
+    PerAgentPreference::entry_def()
 ];
 
 pub fn error<T>(reason: &str) -> ExternResult<T> {
@@ -38,11 +31,40 @@ fn get_all_messages(_: ()) -> ExternResult<MessageListWrapper> {
 }
 
 #[hdk_extern]
-fn get_all_messages_from_addresses(agent_list: AgentListWrapper) -> ExternResult<MessagesByAgentListWrapper> {
+fn get_all_messages_from_addresses(
+    agent_list: AgentListWrapper,
+) -> ExternResult<MessagesByAgentListWrapper> {
     message::handlers::get_all_messages_from_addresses(agent_list)
 }
 
 #[hdk_extern]
-fn get_batch_messages_on_conversation(message_range: MessageRange) -> ExternResult<MessageListWrapper> {
+fn get_batch_messages_on_conversation(
+    message_range: MessageRange,
+) -> ExternResult<MessageListWrapper> {
     message::handlers::get_batch_messages_on_conversation(message_range)
+}
+
+#[hdk_extern]
+fn typing(typing_info: TypingInfo) -> ExternResult<()> {
+    message::handlers::typing(typing_info)
+}
+
+#[hdk_extern]
+fn get_preference(_: ()) -> ExternResult<PreferenceWrapper> {
+    handlers::get_preference()
+}
+
+#[hdk_extern]
+fn set_preference(preference: PreferenceIO) -> ExternResult<()> {
+    handlers::set_preference(preference)
+}
+
+#[hdk_extern]
+fn set_per_agent_preference(preference: PerAgentPreferenceIO) -> ExternResult<()> {
+    handlers::set_per_agent_preference(preference)
+}
+
+#[hdk_extern]
+fn get_per_agent_preference(_: ()) -> ExternResult<PerAgentPreferenceWrapper> {
+    handlers::get_per_agent_preference()
 }
