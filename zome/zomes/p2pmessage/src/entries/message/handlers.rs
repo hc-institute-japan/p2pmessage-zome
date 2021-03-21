3cb0b51f222679f028de78278ee8fa038e5ed09f
Nicko Pangarungan <nickolie.pangarungan@gmail.com>
2021-03-21 22:47:37 +0800

    implement commons types

--- zome/zomes/p2pmessage/src/entries/message/handlers.rs
+++ zome/zomes/p2pmessage/src/entries/message/handlers.rs
@@ -3,7 +3,7 @@ use super::*;
 use crate::timestamp::Timestamp;
 use crate::utils::try_from_element;
 
-// use hdk3::prelude::*;
+use file_types::{FileMetadata, Payload, PayloadInput};
 
 use std::collections::HashMap;
 
@@ -85,13 +85,52 @@ fn init(_: ()) -> ExternResult<InitCallbackResult> {
 pub(crate) fn send_message(message_input: MessageInput) -> ExternResult<MessageAndReceipt> {
     // TODO: check if receiver is blocked
 
-    let message = P2PMessage::from_input(message_input.clone())?;
+    let now = sys_time()?;
 
-    let file = match message_input.payload {
-        PayloadInput::File { .. } => Some(P2PFileBytes::from_input(message_input)?),
-        _ => None,
+    let message = P2PMessage {
+        author: agent_info()?.agent_latest_pubkey,
+        receiver: message_input.receiver,
+        payload: match message_input.payload {
+            PayloadInput::Text { ref payload } => Payload::Text {
+                payload: payload.to_owned(),
+            },
+            PayloadInput::File {
+                ref metadata,
+                ref file_type,
+                ref file_bytes,
+            } => {
+                let p2pfile = P2PFileBytes(file_bytes.clone());
+                create_entry(&p2pfile)?;
+                let file_hash = hash_entry(&p2pfile)?;
+                Payload::File {
+                    metadata: FileMetadata {
+                        file_name: metadata.file_name.clone(),
+                        file_size: metadata.file_size.clone(),
+                        file_type: metadata.file_type.clone(),
+                        file_hash: file_hash,
+                    },
+                    file_type: file_type.clone(),
+                }
+            }
+        },
+        time_sent: Timestamp(now.as_secs() as i64, now.subsec_nanos()),
+        reply_to: message_input.reply_to,
     };
 
+    let file = match message_input.payload {
+        PayloadInput::Text { .. } => None,
+        PayloadInput::File { file_bytes, .. } => Some(P2PFileBytes(file_bytes)),
+    };
+
+    // // create file here
+    // let mut file_hash = None;
+    // if let Some(file) = file {
+    //     file_hash = Some(create_entry(&file)?);
+    // };
+
+    // let message = P2PMessage::from_input(message_input.clone(), None)?;
+
+    // create message input to receive function of recipient
     let receive_input = ReceiveMessageInput(message.clone(), file.clone());
 
     let receive_call_result: Result<P2PMessageReceipt, HdkError> = call_remote(
@@ -105,11 +144,11 @@ pub(crate) fn send_message(message_input: MessageInput) -> ExternResult<MessageA
     match receive_call_result {
         Ok(receive_output) => {
             let receipt = receive_output;
+            // create message entry pointing to file
             create_entry(&message)?;
+            // create receipt
             create_entry(&receipt)?;
-            if let Some(file) = file {
-                create_entry(&file)?;
-            };
+
             // TODO: CREATE AND RETURN ELEMENT HERE
             Ok(MessageAndReceipt(message, receipt))
         }
